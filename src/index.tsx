85ad79d62ac786d8583bb085edd5666d9bb1d592
Your Name <you@example.com>
2023-05-25 20:46:29 +0300

    створила компонент

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,41 @@ import './index.css'
 // const title = React.createElement('h1', { id: 'title' }, 'Hello React')
 
 // JSX
-const app = (
-    <div>
-        <h1 id="text">Hello App</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolore
-            quisquam sunt dolores vel doloribus molestias fugit aliquam impedit.
-            Maxime ullam reprehenderit ea sapiente sequi id consequatur quod
-            magni deserunt!
-        </p>
-    </div>
-)
+// const app = (
+//     <div>
+//         <h1 id="text">Hello App</h1>
+//         <p>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolore
+//             quisquam sunt dolores vel doloribus molestias fugit aliquam impedit.
+//             Maxime ullam reprehenderit ea sapiente sequi id consequatur quod
+//             magni deserunt!
+//         </p>
+//     </div>
+// )
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello App component</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                minima voluptatem vero hic, in soluta distinctio accusantium
+                sapiente cum vel autem debitis adipisci earum expedita neque
+                modi molestias praesentium inventore.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+                minima voluptatem vero hic, in soluta distinctio accusantium
+                sapiente cum vel autem debitis adipisci earum expedita neque
+                modi molestias praesentium inventore.
+            </p>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{app}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
