6b0cb8a1d673b7c20ade98cca7232e6a3c140f09
Your Name <you@example.com>
2023-05-25 20:22:42 +0300

    додала на сторінку рядок та абзац

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 // React Element
-const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+// const title = React.createElement('h1', { id: 'title' }, 'Hello React')
+
+// JSX
+const app = (
+    <div>
+        <h1 id="text">Hello App</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dolore
+            quisquam sunt dolores vel doloribus molestias fugit aliquam impedit.
+            Maxime ullam reprehenderit ea sapiente sequi id consequatur quod
+            magni deserunt!
+        </p>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{app}</React.StrictMode>)
