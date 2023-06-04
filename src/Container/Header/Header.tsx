31f2fd5bbb55d6f38378d9955211193048864921
Your Name <you@example.com>
2023-06-04 18:26:19 +0300

    додала CartHeader

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -6,11 +6,20 @@ import { Container } from '@mui/material'
 import Logo from 'components/Logo/Logo'
 import Menu from 'components/Menu/Menu'
 import './Header.scss'
+import CartHeader from 'components/CartHeader/CartHeader'
 
 type Props = {}
+
 const Header = (props: Props) => {
+    // const appBar = true
     return (
-        <AppBar position="static">
+        <AppBar
+            position="static"
+            className="app-bar"
+            // sx={{ backgroundColor: 'green' }}
+            // style={{ backgroundColor: appBar ? 'green' : 'red' }}
+            // style={{ backgroundColor: 'green' }}
+        >
             <Container>
                 <Toolbar>
                     <IconButton
@@ -24,6 +33,7 @@ const Header = (props: Props) => {
                     </IconButton>
                     <Logo />
                     <Menu />
+                    <CartHeader />
                 </Toolbar>
             </Container>
         </AppBar>
