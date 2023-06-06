08374f88fceff551b54627e8a56bcc1ef8262fb9
Your Name <you@example.com>
2023-06-06 19:00:06 +0300

    додала productsArray в ProductList

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -1,5 +1,6 @@
 import { Grid, Typography } from '@mui/material'
 import ProductListItem from './ProductListItem'
+import productsArray from 'utils/productsArray'
 
 type Props = {}
 const ProductList = (props: Props) => {
@@ -16,15 +17,17 @@ const ProductList = (props: Props) => {
                 List of Products
             </Typography>
             <Grid container spacing={4}>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    <ProductListItem />
-                </Grid>
+                {productsArray.map((product) => (
+                    <Grid item xs={12} sm={6} md={4}>
+                        <ProductListItem
+                            title={product.title}
+                            decscription={product.decscription}
+                            capacity={product.capacity}
+                            type={product.type}
+                            price={product.price}
+                        />
+                    </Grid>
+                ))}
             </Grid>
         </>
     )
---
+++ src/utils/productsArray.ts
@@ -0,0 +1,54 @@
+type Product = {
+    title: string
+    decscription: string
+    capacity: string
+    type: string
+    price: number
+}
+
+const productsArray: Product[] = [
+    {
+        title: 'iPhone 14 Pro',
+        decscription: 'This is iPhone 14 Pro',
+        capacity: 'phone',
+        type: '256',
+        price: 1500,
+    },
+    {
+        title: 'iPhone 13Pro',
+        decscription: 'This is iPhone 13 Pro',
+        capacity: 'phone',
+        type: '256',
+        price: 1000,
+    },
+    {
+        title: 'iPhone 14 ',
+        decscription: 'This is iPhone 14 ',
+        capacity: 'phone',
+        type: '512',
+        price: 2000,
+    },
+    {
+        title: 'iPhone 11 Max ',
+        decscription: 'This is 11 Max ',
+        capacity: 'phone',
+        type: '128',
+        price: 1900,
+    },
+    {
+        title: 'iPhone XS ',
+        decscription: 'This is iPhone XS ',
+        capacity: 'phone',
+        type: '68',
+        price: 500,
+    },
+    {
+        title: 'iPhone 12 Pro ',
+        decscription: 'This is iPhone 12 Pro ',
+        capacity: 'phone',
+        type: '664',
+        price: 3000,
+    },
+]
+
+export default productsArray
