15acab40f476156c893ef6863604509dd8d7bd2a
Your Name <you@example.com>
2023-06-27 13:11:14 +0300

    не виводяться товари в card

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,12 +1,14 @@
 import CartProductList from 'components/CartProductList/CartProductList'
 import CartTotal from 'components/CartTotal/CartTotal'
+import { useAppSelector } from 'redux/hooks'
 
 type Props = {
-    productsInCart: {
+    productsInCart?: {
         [id: number]: number
     }
 }
-const CartHeader = ({ productsInCart }: Props) => {
+const CartHeader = (props: Props) => {
+    const productsInCart = useAppSelector((state) => state.productsInCart)
     return (
         <div>
             <div>
--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -2,13 +2,16 @@ import { Grid } from '@mui/material'
 import CartProductList from 'components/CartProductList/CartProductList'
 import CartProductlistItemExtended from 'components/CartProductList/CartProductListItemExtended'
 import CartTotal from 'components/CartTotal/CartTotal'
+import { useAppSelector } from 'redux/hooks'
 
 type Props = {
-    productsInCart: {
+    productsInCart?: {
         [id: number]: number
     }
 }
-const CartPage = ({ productsInCart }: Props) => {
+const CartPage = (props: Props) => {
+    const productsInCart = useAppSelector((state) => state.productsInCart)
+
     return (
         <div>
             <Grid container spacing={4}>
